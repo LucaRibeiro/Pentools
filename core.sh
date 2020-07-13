0d0b7641e27e9e97f93a234cc91eeca5d473a857
LucaRibeiro <luca.rsr.luca@gmail.com>
2020-07-13 16:58:59 -0300

    Core/Essentials implemented

--- core.sh
+++ core.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
-
 sudo apt update
 sudo apt upgrade -y
+sudo apt install dpkg -y
 
-if  ! sudo dpkg -l | grep -q 'python3';then
-    sudo apt install python3
-
-if  ! sudo dpkg -l | grep -q 'python-minimal';then
-    sudo apt install python-minimal
-
-if  ! sudo dpkg -l | grep -q 'git';then
+if ! sudo dpkg -l | grep -q 'python3'; then
+	sudo apt install python3
+fi
+####################################################################################################################################################
+if ! sudo dpkg -l | grep -q 'python-minimal'; then
+	sudo apt install python-minimal
+fi
+####################################################################################################################################################
+if ! sudo dpkg -l | grep -q 'git'; then
 	sudo apt install git -y
-
-if  ! sudo dpkg -l | grep -q 'curl';then
+fi
+####################################################################################################################################################
+if ! sudo dpkg -l | grep -q 'curl'; then
 	sudo apt install curl -y
-
-if  ! sudo dpkg -l | grep -q 'vim';then
+fi
+####################################################################################################################################################
+if ! sudo dpkg -l | grep -q 'vim'; then
 	sudo apt install vim -y
-
-if  ! sudo dpkg -l | grep -q 'wget';then
+fi
+####################################################################################################################################################
+if ! sudo dpkg -l | grep -q 'wget'; then
 	sudo apt install wget -y
-
-clear
-
+fi
+####################################################################################################################################################
 sudo bash ./essentials.sh
--- essentials.sh
+++ essentials.sh
@@ -2,62 +2,67 @@
 GREEN="\033[1;32m"
 RED="\033[1;31m"
 RESET="\033[0;0m"
-
-
-#####################################################################################################################################################
+##################################################################################################################################################
 printf "${GREEN} Installing netcat ${RESET} \n"
-if  ! sudo apt list --installed | grep -q 'netcat';then
+if ! sudo dpkg -l | grep -q 'netcat'; then
 	sudo apt install netcat -y
-else
-	printf "${RED} Is already installed ${RESET} \n"
+fi
 ####################################################################################################################################################
 printf "${GREEN} Installing nmap ${RESET} \n"
-if  ! sudo apt list --installed | grep -q 'nmap';then
+if ! sudo dpkg -l | grep -q 'nmap'; then
 	sudo apt install nmap -y
-else
-	printf "${RED} Is already installed ${RESET} \n"
+fi
 ####################################################################################################################################################
 printf "${GREEN} Installing wfuzz ${RESET} \n"
-if  ! sudo apt list --installed | grep -q '';then
+if ! sudo dpkg -l | grep -q 'wfuzz'; then
 	sudo apt install wfuzz -y
-else
-	printf "${RED} Is already installed ${RESET} \n"
+fi
 ####################################################################################################################################################
 printf "${GREEN} Installing hydra ${RESET} \n"
-if  ! sudo apt list --installed | grep -q '';then
+if ! sudo dpkg -l | grep -q 'hydra'; then
 	sudo apt install hydra -y
-else
-	printf "${RED} Is already installed ${RESET} \n"
+fi
 ####################################################################################################################################################
 printf "${GREEN} Installing sqlmap ${RESET} \n"
-cd /opt && git clone --depth 1 https://github.com/sqlmapproject/sqlmap.git sqlmap-dev
-export SQLMAP=/opt/sqlmap-dev
-export PATH=$PATH:$SQLMAP
+if ! sudo dpkg -l | grep -q ''; then
+	cd /opt && git clone --depth 1 https://github.com/sqlmapproject/sqlmap.git sqlmap-dev
+	export SQLMAP=/opt/sqlmap-dev
+	export PATH=$PATH:$SQLMAP
+fi
 ####################################################################################################################################################
 printf "${GREEN} Installing git-dumper ${RESET} \n"
-cd /opt && git clone https://github.com/arthaud/git-dumper.git
-pip install -r git-dumper/requirements.txt
-export GIT_DUMPER=/opt/git-dumper
-export PATH=$PATH:$GIT_DUMPER
+if ! ls /opt | grep -q 'git-dumper'; then
+	cd /opt && git clone https://github.com/arthaud/git-dumper.git
+	pip install -r git-dumper/requirements.txt
+	export GIT_DUMPER=/opt/git-dumper
+	export PATH=$PATH:$GIT_DUMPER
+fi
 ####################################################################################################################################################
 printf "${GREEN} Installing Metasploit ${RESET} \n"
-curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall
-chmod 755 msfinstall
-./msfinstall
-rm -rf ./msfinstall
-clear
+if ! sudo dpkg -l | grep -q 'metasploit'; then
+	curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb >msfinstall
+	chmod 755 msfinstall
+	./msfinstall
+	rm -rf ./msfinstall
+	clear
+fi
 ####################################################################################################################################################
 printf "${GREEN} Installing WPScan ${RESET} \n"
-sudo apt-get install libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ruby-dev build-essential libgmp-dev zlib1g-dev
-cd /opt && git clone https://github.com/wpscanteam/wpscan.git
-cd wpscan
-sudo gem install bundler && bundle install --without test development -y
+if ! ls /opt | grep -q 'wpscan'; then
+	sudo apt-get install libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ruby-dev build-essential libgmp-dev zlib1g-dev
+	cd /opt && git clone https://github.com/wpscanteam/wpscan.git
+	cd wpscan
+	sudo gem install bundler && bundle install --without test development -y
+fi
 ####################################################################################################################################################
 printf "${GREEN} Installing JohnTheRipper ${RESET} \n"
-cd /opt && sudo git clone https://github.com/magnumripper/JohnTheRipper
-export JOHN_THE_RIPPER=/opt/JohnTheRipper
-export PATH=$PATH:$JOHN_THE_RIPPER
+if ! ls /opt | grep -q 'JohnTheRipper'; then
+	cd /opt && sudo git clone https://github.com/magnumripper/JohnTheRipper
+	export JOHN_THE_RIPPER=/opt/JohnTheRipper
+	export PATH=$PATH:$JOHN_THE_RIPPER
+fi
 #####################################################################################################################################################
 printf "${GREEN} Adding SecList ${RESET} \n"
-cd /opt && sudo git clone https://github.com/danielmiessler/SecLists.git
-####################################################################################################################################################
+if ! ls /opt | grep -q 'SecList'; then
+	cd /opt && sudo git clone https://github.com/danielmiessler/SecLists.git
+fi
