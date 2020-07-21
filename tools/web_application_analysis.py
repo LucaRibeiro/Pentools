def07440d11b6ddffef4223b210e54e8333ece07
LucaRibeiro <luca.rsr.luca@gmail.com>
2020-07-21 14:32:20 -0300

    Web application analysis repositories added

--- tools/web_application_analysis.py
+++ tools/web_application_analysis.py
@@ -2,218 +2,261 @@
 
 tools = [
     {
-        "package":"apache-users",
-        "instalation": "",
+        "package": "apache-users",
+        "instalation": "Download unavailable.",
+        "essential-tool": False,
         "requiresUI": False
     },
     {
         "package": "Arachni",
-        "instalation": "",
+        "instalation": "sudo apt-get install build-essential-tool curl libcurl4 libcurl4-openssl-dev ruby ruby-dev & sudo gem install arachni",
+        "essential-tool": False,
         "requiresUI": False
     },
     {
         "package": "BBQSQL",
-        "instalation": "",
+        "instalation": "sudo pip install bbqsql",
+        "essential-tool": False,
         "requiresUI": False
     },
     {
         "package": "BlindElephant",
-        "instalation": "",
+        "instalation": "cd /opt & sudo git clone https://github.com/lokifer/BlindElephant/blob/master/src/setup.py & cd blindelephant/src & sudo python setup.py install",
+        "essential-tool": False,
         "requiresUI": False
     },
     {
         "package": "Burp Suite",
-        "instalation": "",
-        "requiresUI": False
+        "instalation": "UI requires.",
+        "essential-tool": False,
+        "requiresUI": True
     },
     {
         "package": "CutyCapt",
-        "instalation": "",
+        "instalation": "Download unavailable.",
+        "essential-tool": False,
         "requiresUI": False
     },
     {
         "package": "DAVTest",
-        "instalation": "",
+        "instalation": "Download unavailable.",
+        "essential-tool": False,
         "requiresUI": False
     },
     {
         "package": "deblaze",
-        "instalation": "",
+        "instalation": "Download unavailable.",
+        "essential-tool": False,
         "requiresUI": False
     },
     {
         "package": "DIRB",
-        "instalation": "",
+        "instalation": "sudo apt install dirb -y",
+        "essential-tool": False,
         "requiresUI": False
     },
     {
         "package": "DirBuster",
-        "instalation": "",
-        "requiresUI": False
+        "instalation": "UI requires.",
+        "essential-tool": False,
+        "requiresUI": True
     },
     {
         "package": "fimap",
-        "instalation": "",
+        "instalation": "Download unavailable.",
+        "essential-tool": False,
         "requiresUI": False
     },
     {
         "package": "FunkLoad",
-        "instalation": "",
+        "instalation": "Download unavailable.",
+        "essential-tool": False,
         "requiresUI": False
     },
     {
         "package": "Gobuster",
-        "instalation": "",
+        "instalation": "sudo apt install gobuster -y",
+        "essential-tool": True,
         "requiresUI": False
     },
     {
         "package": "Grabber",
-        "instalation": "",
+        "instalation": "Download unavailable.",
+        "essential-tool": False,
         "requiresUI": False
     },
     {
         "package": "hURL",
-        "instalation": "",
+        "instalation": "sudo apt-get install hurl -y'",
+        "essential-tool": False,
         "requiresUI": False
     },
     {
         "package": "jboss-autopwn",
-        "instalation": "",
+        "instalation": "Download unavailable.",
+        "essential-tool": False,
         "requiresUI": False
     },
     {
         "package": "joomscan",
-        "instalation": "",
+        "instalation": "sudo apt-get install joomscan -y'",
+        "essential-tool": False,
         "requiresUI": False
     },
     {
         "package": "jSQL Injection",
-        "instalation": "",
-        "requiresUI": False
+        "instalation": "UI requires.",
+        "essential-tool": False,
+        "requiresUI": True
     },
     {
         "package": "Maltego Teeth",
-        "instalation": "",
+        "instalation": "mkdir ~/Maltego && cd ~/Maltego &&  sudo wget https://maltego-downloads.s3.us-east-2.amazonaws.com/linux/Maltego.v4.2.11.13104.deb && sudo dpkg –i Maltego.v4.2.11.13104.deb && cd .. && sudo rm -rf Maltego",
+        "essential-tool": False,
         "requiresUI": False
     },
     {
         "package": "Nikto",
-        "instalation": "",
+        "instalation": "sudo apt install -y nikto",
+        "essential-tool": True,
         "requiresUI": False
     },
     {
         "package": "PadBuster",
-        "instalation": "",
+        "instalation": "Download unavailable.",
+        "essential-tool": False,
         "requiresUI": False
     },
     {
         "package": "Paros",
-        "instalation": "",
-        "requiresUI": False
+        "instalation": "UI requires.",
+        "essential-tool": False,
+        "requiresUI": True
     },
     {
         "package": "Parsero",
-        "instalation": "",
+        "instalation": "sudo pip3 install parsero",
+        "essential-tool": False,
         "requiresUI": False
     },
     {
         "package": "plecost",
-        "instalation": "",
+        "instalation": "sudo pip3 install plecost",
+        "essential-tool": False,
         "requiresUI": False
     },
     {
         "package": "Powerfuzzer",
-        "instalation": "",
-        "requiresUI": False
+        "instalation": "UI requires.",
+        "essential-tool": False,
+        "requiresUI": True
     },
     {
         "package": "ProxyStrike",
-        "instalation": "",
-        "requiresUI": False
+        "instalation": "UI requires.",
+        "essential-tool": False,
+        "requiresUI": True
     },
     {
         "package": "Recon-ng",
-        "instalation": "",
+        "instalation": "sudo apt install recon-ng -y",
+        "essential-tool": False,
         "requiresUI": False
     },
     {
         "package": "Skipfish",
-        "instalation": "",
+        "instalation": "sudo apt install skipfish -y",
+        "essential-tool": False,
         "requiresUI": False
     },
     {
         "package": "sqlmap",
-        "instalation": "",
+        "instalation": "sudo apt install sqlmap -y",
+        "essential-tool": True,
         "requiresUI": False
     },
     {
         "package": "Sqlninja",
-        "instalation": "",
+        "instalation": "sudo apt install sqlninja -y",
+        "essential-tool": False,
         "requiresUI": False
     },
     {
         "package": "sqlsus",
-        "instalation": "",
+        "instalation": "sudo apt install sqlsus -y",
+        "essential-tool": False,
         "requiresUI": False
     },
     {
         "package": "ua-tester",
-        "instalation": "",
+        "instalation": "Download unavailable.",
+        "essential-tool": False,
         "requiresUI": False
     },
     {
         "package": "Uniscan",
-        "instalation": "",
+        "instalation": "sudo apt install uniscan -y",
+        "essential-tool": False,
         "requiresUI": False
     },
     {
         "package": "w3af",
-        "instalation": "",
-        "requiresUI": False
+        "instalation": "UI requires.",
+        "essential-tool": False,
+        "requiresUI": True
     },
     {
         "package": "WebScarab",
-        "instalation": "",
-        "requiresUI": False
+        "instalation": "UI requires.",
+        "essential-tool": False,
+        "requiresUI": True
     },
     {
         "package": "Webshag",
-        "instalation": "",
+        "instalation": "Download unavailable.",
+        "essential-tool": False,
         "requiresUI": False
     },
     {
         "package": "WebSlayer",
-        "instalation": "",
-        "requiresUI": False
+        "instalation": "UI requires.",
+        "essential-tool": False,
+        "requiresUI": True
     },
     {
         "package": "WebSploit",
-        "instalation": "",
+        "instalation": "sudo apt install websploit -y",
+        "essential-tool": False,
         "requiresUI": False
     },
     {
         "package": "Wfuzz",
-        "instalation": "",
+        "instalation": "sudo apt install wfuzz -y",
+        "essential-tool": True,
         "requiresUI": False
     },
     {
         "package": "WhatWeb",
-        "instalation": "",
+        "instalation": "sudo apt install whatweb -y",
+        "essential-tool": True,
         "requiresUI": False
     },
     {
         "package": "WPScan",
-        "instalation": "",
+        "instalation": "sudo apt install wpscan -y",
+        "essential-tool": True,
         "requiresUI": False
     },
     {
         "package": "XSSer",
-        "instalation": "",
+        "instalation": "sudo apt install xsser -y",
+        "essential-tool": False,
         "requiresUI": False
     },
     {
         "package": "zaproxy",
-        "instalation": "",
-        "requiresUI": False
+        "instalation": "UI requires.",
+        "essential-tool": False,
+        "requiresUI": True
     }
 ]
