df8e9388a7d31e4f3faf308c17167cdd7a4bac89
LucaRibeiro <luca.rsr.luca@gmail.com>
2020-07-24 13:43:35 -0300

    Methods to install without ui, install with ui and install all implemented

--- src/category.py
+++ src/category.py
@@ -12,18 +12,28 @@ class Category():
     def getTool(self, toolName):
         for tool in self.tools:
             if tool.get('name') == toolName:
-                return tool.get
+                return tool
+
+    def installTool(self, tool):
+        print("\033[1;32mInstalando {}\033[0;0m".format(tool.get('package')))
+        codigo = tool.get('installation')
+        os.system(codigo)
 
     def installWithoutUI(self):
         for tool in self.tools:
-            if tool.get('requiresUI') != True:
-                print("\033[1;32mInstalando {}\033[0;0m".format(
-                    tool.get('package')))
-                codigo = tool.get('installation')
-                os.system(codigo)
+            if not(tool.get('requiresUI')):
+                self.installTool(tool)
 
-    def installWithUI(self):
+    def installAll(self):
         for tool in self.tools:
-            print("Instalando " + tool.get('package'))
-            codigo = tool.get('installation')
-            os.system(codigo)
+            self.installTool(tool)
+
+    def installEssentialsWithoutUI(self):
+        for tool in self.tools:
+            if not(tool.get('RequiresUI')) and tool.get('essential-tool'):
+                self.installTool(tool)
+
+    def installEssentials(self):
+        for tool in self.tools:
+            if tool.get('essential-tool'):
+                self.installTool(tool)
