9806617d1955a6f80c8148b533f07dfbb7a7f0c8
LucaRibeiro <luca.rsr.luca@gmail.com>
2020-08-03 10:55:05 -0300

    Create class

--- src/categories/hardware_hacking.py
+++ src/categories/hardware_hacking.py
@@ -3,44 +3,44 @@
 tools = [
     {
         "package": "android-sdk",
-        "essential-tool": False,
         "installation": "",
-        "requiresUI": False,
+        "essential-tool": false,
+        "requiresUI": false,
         "categories": ["hardware hacking"]
     },
     {
         "package": "apktool",
-        "essential-tool": False,
         "installation": "",
-        "requiresUI": False,
+        "essential-tool": false,
+        "requiresUI": false,
         "categories": ["hardware hacking"]
     },
     {
         "package": "Arduino",
-        "essential-tool": False,
         "installation": "",
-        "requiresUI": False,
+        "essential-tool": false,
+        "requiresUI": false,
         "categories": ["hardware hacking"]
     },
     {
         "package": "dex2jar",
-        "essential-tool": False,
         "installation": "",
-        "requiresUI": False,
+        "essential-tool": false,
+        "requiresUI": false,
         "categories": ["hardware hacking"]
     },
     {
         "package": "Sakis3G",
-        "essential-tool": False,
         "installation": "",
-        "requiresUI": False,
+        "essential-tool": false,
+        "requiresUI": false,
         "categories": ["hardware hacking"]
     },
     {
         "package": "smali",
-        "essential-tool": False,
         "installation": "",
-        "requiresUI": False,
+        "essential-tool": false,
+        "requiresUI": false,
         "categories": ["hardware hacking"]
     }
 ]
